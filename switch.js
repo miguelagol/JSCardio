0faf0ca42361ce396342131ee3b1ee3e2eeeeee5
Michalina <michalina.gol@gmail.com>
2018-09-24 17:55:24 +0200

    Add comments and examples of the switch statement usage

--- switch.js
+++ switch.js
@@ -1,43 +1,126 @@
-/*switch (browser) {
-    case 'Edge':
-      alert( "You've got the Edge!" );
-      break;
-  
-    case 'Chrome':
-    case 'Firefox':
-    case 'Safari':
-    case 'Opera':
-      alert( 'Okay we support these browsers too' );
-      break;
-  
-    default:
-      alert( 'We hope that this page looks ok!' );
-  } */
+// A switch statement can replace multiple if checks.
+/*  switch(x) {
+        case 'value1':  // if (x === 'value1')
+        ...
+        [break]
+    
+        case 'value2':  // if (x === 'value2')
+        ...
+        [break]
+    
+        default:
+        ...
+        [break]
+    }
+*/
+let a = 2 + 2;
 
-if (browser=='Edge') {
-    alert("You've got the Edge!");
+switch (a) {
+    case 3:
+        console.log('Too small');
+        break;
+    case 4:
+        console.log('Exactly!');
+        break;
+    case 5:
+        console.log('Too large');
+        break;
+    default:
+        console.log("I don't know such values");
+}   // Exactly!
+
+//--------------------REMEMBER--------------------
+// If there is no break then the execution continues with the next case without any checks.
+
+switch (a) {
+    case 3:
+        console.log('Too small');
+    case 4:
+        console.log('Exactly!');
+    case 5:
+        console.log('Too large');
+    default:
+        console.log("I don't know such values");
+}   // Exactly!, Too large, I don't know such values
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Grouping of "case"
+switch (a) {
+    case 4:
+        console.log('Right!');
+        break;
+    case 3:            // grouped two cases
+    case 5:
+        console.log('Wrong!');
+        console.log("Why don't you take a math class?");
+        break;
+    default:
+        console.log('The result is strange. Really.');
 }
-else if (browser=='Chrome' || browser=='Firefox' || browser=='Safari' || browser=='Opera') {
-    alert('Okay we support these browsers too');
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Type metters
+// The values must be of the same type to match.
+
+let arg = prompt("Enter a value?")
+switch (arg) {
+    case '0':
+    case '1':
+        console.log('One or zero');
+        break;
+    case '2':
+        console.log('Two');
+        break;
+    case 3:
+        console.log('Never executes!'); // typeof(arg) = string => '3' != 3
+        break;
+    default:
+        console.log('An unknown value')
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// TASK 1 - Rewrite the "switch" into an "if"
+/*  switch (browser) {
+        case 'Edge':
+            console.log( "You've got the Edge!" );
+            break;
+        case 'Chrome':
+        case 'Firefox':
+        case 'Safari':
+        case 'Opera':
+            console.log( 'Okay we support these browsers too' );
+            break;
+        default:
+            console.log( 'We hope that this page looks ok!' );
+  } */
+if (browser == 'Edge') {
+    console.log("You've got the Edge!");
+}
+else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
+    console.log('Okay we support these browsers too');
 }
 else {
-    alert('We hope that this page looks ok!');
+    console.log('We hope that this page looks ok!');
 }
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
 
-/*let a = +prompt('a?', '');
+// TASK 2 - Rewrite "if" into "switch"
+/*  let a = +prompt('a?', '');
 
-if (a == 0) {
-  alert( 0 );
-}
-if (a == 1) {
-  alert( 1 );
-}
-
-if (a == 2 || a == 3) {
-  alert( '2,3' );
-}*/
-
+    if (a == 0) {
+        console.log( 0 );
+    }
+    if (a == 1) {
+        console.log( 1 );
+    }
+    if (a == 2 || a == 3) {
+        console.log( '2,3' );
+    }
+*/
 let a = +prompt('a?', '');
 
 switch (a) {
