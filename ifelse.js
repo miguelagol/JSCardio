41e74ffcbba3ad62a09c0af730f1f2cae755a937
Michalina Gołąb <michalina.gol@gmail.com>
2018-09-26 12:38:17 +0000

    Delete ifelse.js file

--- ifelse.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-let result = (a+b<4) ? 'Below' : 'Over';
-
-let  message = 
-    (login == 'Employee') ? 'Hello' :
-    (login == 'Director') ? 'Greetings' :
-    (login == '') ? 'No login': '';
