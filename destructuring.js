826e7ac5c7d21a3073c2975ad3ae87206a81e7a4
Michalina <michalina.gol@gmail.com>
2018-11-19 16:36:38 +0100

    Add informations and examples about nested destructuring and one task

--- destructuring.js
+++ destructuring.js
@@ -45,7 +45,7 @@ let user = {
 };
 
 for (let [key, value] of Object.entries(user)) {
-   console.log(`${key}: ${value}`);    // name: Jack, age: 15
+   console.log(`${key}: ${value}`); // name: Jack, age: 15
 }
 
 let user2 = new Map();
@@ -53,13 +53,18 @@ user2.set('name', 'Jack');
 user2.set('age', 15);
 
 for (let [key, value] of user2.entries()) {
-   console.log(`${key}: ${value}`);    // name: Jack, age: 15
+   console.log(`${key}: ${value}`); // name: Jack, age: 15
 }
 
 //--------------------------------------------------------------------------------------
 
 // The rest '...' operator
-let [name1, name2, ...rest] = ['Julius', 'Cesar', 'Consul', 'of the Roman Republic'];
+let [name1, name2, ...rest] = [
+   'Julius',
+   'Cesar',
+   'Consul',
+   'of the Roman Republic',
+];
 
 console.log(name2); // Cesar
 console.log(rest[0]); // Consul
@@ -82,7 +87,7 @@ console.log(surname2); // Anonymous    (default value)
 // OBJECT DESTRUCTURING
 // let {var1, var2} = {var1: ..., var2: ...}
 let options = {
-   title: "Menu",
+   title: 'Menu',
    width: 100,
    height: 200,
 };
@@ -94,7 +99,7 @@ console.log(width); // 100
 console.log(options.height); // 200
 
 // the order does not metter
-let { height, width, title } = { title: "Menu", height: 200, width: 100 }
+let { height, width, title } = { title: 'Menu', height: 200, width: 100 };
 
 console.log(title); // Menu
 console.log(width); // 100
@@ -102,12 +107,12 @@ console.log(width); // 100
 // we can assign a property to a variable with another name
 // let {sourceProperty: targetVariable} = {sourceProperty: ...}
 let options = {
-   title: "Menu",
+   title: 'Menu',
    width: 100,
    height: 200,
 };
 
-let {width: wi, height: he, title} = options;
+let { width: wi, height: he, title } = options;
 
 console.log(wi); // 100
 console.log(he); // 200
@@ -117,10 +122,10 @@ console.log(title); // Menu
 
 // Default values
 let options = {
-   title: "Menu"
+   title: 'Menu',
 };
 
-let {width = 100, title} = options;
+let { width = 100, title } = options;
 
 console.log(title); // Menu
 console.log(width); // 100
@@ -129,7 +134,7 @@ let options = {
    width: 200,
 };
 
-let {title = "Menu", width: wi = 300, height: he = 100} = options;
+let { title = 'Menu', width: wi = 300, height: he = 100 } = options;
 
 console.log(title); // Menu
 console.log(wi); // 200
@@ -139,12 +144,12 @@ console.log(he); // 100
 
 // The rest '...' operator
 let options = {
-   title: "Menu",
+   title: 'Menu',
    width: 100,
-   height: 200
+   height: 200,
 };
 
-let {title, ...rest} = options;
+let { title, ...rest } = options;
 
 console.log(rest.width); // 100
 console.log(rest.height); // 200
@@ -155,28 +160,109 @@ console.log(rest.height); // 200
 
 let title, width, height;
 
-// JavaScript treats {...} in the main code flow (not inside another expression) as a code block
-{title, width, height} = {title: "Menu", width: 200, height: 100}; 
-
-console.log(title); // Error: Unexpected token =
+/*    JavaScript treats {...} in the main code flow (not inside another expression) as a code block
+      {title, width, height} = {title: "Menu", width: 200, height: 100}; 
+      console.log(title); // Error: Unexpected token =
+*/
 
 // To show JavaScript that it’s not a code block, we can wrap the whole assignment in brackets (...)
 // now ok
 let title, width, height;
 
-({title, width, height} = {title: "Menu", width: 200, height: 100});
+({ title, width, height } = { title: 'Menu', width: 200, height: 100 });
 
 console.log(title); // Menu
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 // Nested destructuring
+let options = {
+   size: {
+      width: 200,
+      height: 100,
+   },
+   items: ['Cake', 'Donut'],
+   extra: true,
+};
 
+// nested destructruring assignment onmultiple lines for clarity
+let {
+   size: { width, height },
+   items: [item1, item2],
+   title = 'Menu',
+} = options;
+
+console.log(width); // 200
+console.log(height); // 100
+console.log(item2); // Donut
+console.log(title); // Menu
+
+let options = {
+   size: {
+      width: 200,
+      height: 100,
+   },
+   items: ['Cake', 'Donut'],
+   title: 'Menu',
+   extra: true,
+};
+
+let { size } = options;
+
+console.log(size); //{ width: 200, height: 100 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 // Smart function parameters
+let options = {
+   title: 'My menu',
+   items: ['Item1', 'Item2'],
+};
 
+/*	function({
+  		incomingProperty: parameterName = defaultValue
+  		...
+	})
+*/
+function showMenu({
+   title = 'Untitled',
+   width = 200,
+   height = 100,
+   items = [],
+}) {
+   console.log(`${title} ${width} ${height}`);
+   console.log(items);
+}
+
+showMenu(options); // My menu 200 100,		[ 'Item1', 'Item2' ]
+showMenu({}); // Untitled 200 100,		[]
+
+showMenu(); // Error: Cannot destructure property `title` of 'undefined' or 'null'
+
+// we can fix it by making {} the default value for the whole destructuring thing
+function showMenu({ title = 'Menu', width = 100, height = 200 } = {}) {
+   console.log(`${title} ${width} ${height}`);
+}
+
+showMenu(); // Menu 100 200
+
+let options = {
+   title: 'My menu',
+   items: ['Item1', 'Item2'],
+};
+
+function showMenu({
+   title = 'Untitled',
+   width: wi = 200,
+   height: he = 100,
+   items: [item1, item2],
+}) {
+   console.log(`${title} ${wi} ${he}`);
+   console.log(item1);
+   console.log(item2);
+}
+
+showMenu(options); // My menu 200 100,		Item1,	Item2
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -212,11 +298,18 @@ console.log(isAdmin2); // false
       -  If there are multiple top-paid persons, return any of them.
 */
 function topSalary(salaries) {
-
+   let maxSalary = 0;
+   let maxName = null;
+   for (let [name, salary] of Object.entries(salaries)) {
+      salary > maxSalary ? ([maxName, maxSalary] = [name, salary]) : null;
+   }
+   console.log(maxName);
 }
 
 let salaries = {
-   'Jack': 100,
-   'Pete': 250,
-   'Ann': 200,
+   Jack: 100,
+   Pete: 250,
+   Ann: 300,
 };
+
+topSalary(salaries);
