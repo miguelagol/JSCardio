a120a0f1e37cd210c2e21744aaba3eb4910d77d3
Michalina <michalina.gol@gmail.com>
2018-11-27 15:41:52 +0100

    Add prettier changes

--- json.js
+++ json.js
@@ -3,11 +3,11 @@
 // JSON.stringify()
 // convert objects into JSON.
 let student = {
-    name: 'Jack',
-    age: 24,
-    isAdmin: false,
-    courses: ['html', 'css', 'js'],
-    wife: null,
+   name: 'Jack',
+   age: 24,
+   isAdmin: false,
+   courses: ['html', 'css', 'js'],
+   wife: null,
 };
 
 // The resulting json string is a called JSON-encoded or serialized or stringified or marshalled object
@@ -34,11 +34,11 @@ console.log(json); // {"name":"Jack","age":24,"isAdmin":false,"courses":["html",
 
 // Nested objects are supported and converted automatically
 let meetup = {
-    title: 'COnference',
-    room: {
-        number: 23,
-        participants: ['John', 'Ann'],
-    },
+   title: 'COnference',
+   room: {
+      number: 23,
+      participants: ['John', 'Ann'],
+   },
 };
 
 console.log(JSON.stringify(meetup)); // {"title":"COnference","room":{"number":23,"participants":["John","Ann"]}}
@@ -48,12 +48,12 @@ console.log(JSON.stringify(meetup)); // {"title":"COnference","room":{"number":2
 //-------------------REMEMBER--------------------
 // The important limitation: there must be no circular references.
 let room = {
-    number: 23,
+   number: 23,
 };
 
 let meetup = {
-    title: 'Conference',
-    participants: ['John', 'Ann'],
+   title: 'Conference',
+   participants: ['John', 'Ann'],
 };
 
 meetup.place = room;
@@ -70,11 +70,11 @@ JSON.stringify(meetup); // Error: Converting circular structure to JSON
     - Properties that store undefined.
 */
 let user = {
-    sayHi() {
-        console.log('Hello');
-    },
-    [Symbol('id')]: 123,
-    something: undefined,
+   sayHi() {
+      console.log('Hello');
+   },
+   [Symbol('id')]: 123,
+   something: undefined,
 };
 
 console.log(JSON.stringify(user)); // {}    (empty object)
@@ -89,13 +89,13 @@ console.log(JSON.stringify(user)); // {}    (empty object)
 */
 // If we pass an array of properties to it, only these properties will be encoded.
 let room = {
-    number: 123,
+   number: 123,
 };
 
 let meetup = {
-    title: 'Conference',
-    participants: [{ name: 'John' }, { name: 'Ann' }],
-    place: room,
+   title: 'Conference',
+   participants: [{ name: 'John' }, { name: 'Ann' }],
+   place: room,
 };
 
 room.occupiedBy = meetup;
@@ -103,14 +103,14 @@ room.occupiedBy = meetup;
 console.log(JSON.stringify(meetup, ['title', 'participants'])); // {"title":"Conference","participants":[{},{}]}
 
 console.log(
-    JSON.stringify(meetup, ['title', 'participants', 'name', 'place', 'number']),
+   JSON.stringify(meetup, ['title', 'participants', 'name', 'place', 'number']),
 ); // {"title":"Conference","participants":[{"name":"John"},{"name":"Ann"}],"place":{"number":123}}
 
 console.log(
-    JSON.stringify(meetup, function replacer(key, value) {
-        console.log(`${key}: ${value}`);
-        return key == 'occupiedBy' ? undefined : value;
-    }),
+   JSON.stringify(meetup, function replacer(key, value) {
+      console.log(`${key}: ${value}`);
+      return key == 'occupiedBy' ? undefined : value;
+   }),
 );
 /* 
 : [object Object]
@@ -130,12 +130,12 @@ occupiedBy: [object Object]
 */
 
 let user = {
-    name: 'John',
-    age: 25,
-    roles: {
-        isAdmin: false,
-        isEditor: true,
-    },
+   name: 'John',
+   age: 25,
+   roles: {
+      isAdmin: false,
+      isEditor: true,
+   },
 };
 
 console.log(JSON.stringify(user, null, 3));
@@ -155,28 +155,28 @@ console.log(JSON.stringify(user, null, 3));
 // toJSON
 // An object may provide method toJSON for to-JSON conversion. JSON.stringify automatically calls it if available.
 let room = {
-    numer: 23,
+   numer: 23,
 };
 
 let meetup = {
-    title: 'Conference',
-    date: new Date(Date.UTC(2017, 0, 1)),
-    room,
+   title: 'Conference',
+   date: new Date(Date.UTC(2017, 0, 1)),
+   room,
 };
 
 // we can see that date became a string. That’s because all dates have a built-in toJSON method which returns such kind of string.
 console.log(JSON.stringify(meetup)); // {"title":"Conference","date":"2017-01-01T00:00:00.000Z","room":{"numer":23}}
 
 let room2 = {
-    number: 12,
-    toJSON() {
-        return this.number;
-    },
+   number: 12,
+   toJSON() {
+      return this.number;
+   },
 };
 
 let meetup2 = {
-    title: 'Conference',
-    room2
+   title: 'Conference',
+   room2,
 };
 
 // toJSON is used both for the direct call JSON.stringify(room) and for the nested object.
@@ -191,12 +191,13 @@ console.log(JSON.stringify(meetup2)); // {"title":"Conference","room2":12}
 /*  str -   JSON-string to parse
     reviver -   Optional function(key,value) that will be called for each (key, value) pair and can transform the value.
 */
-let numbers = "[0, 1, 2, 3]";
+let numbers = '[0, 1, 2, 3]';
 numbers = JSON.parse(numbers);
 
 console.log(numbers[1]); // 1
 
-let user = '{ "name": "John", "age": 35, "isAdmin": false, "friends": [0,1,2,3] }';
+let user =
+   '{ "name": "John", "age": 35, "isAdmin": false, "friends": [0,1,2,3] }';
 user = JSON.parse(user);
 
 console.log(user.friends[1]); // 1
@@ -210,9 +211,9 @@ console.log(meetup.date.getDate()); // Error: meetup.date.getDate is not a funct
 
 let str2 = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
 
-let meetup2 = JSON.parse(str2, function (key, value) {
-    if (key == 'date') return new Date(value);
-    return value;
+let meetup2 = JSON.parse(str2, function(key, value) {
+   if (key == 'date') return new Date(value);
+   return value;
 });
 
 console.log(meetup2.date.getDate()); // 30
@@ -221,8 +222,8 @@ console.log(meetup2.date.getDate()); // 30
 
 // TASK 1 - Turn the object into JSON and back
 let user = {
-    name: "John Smith",
-    age: 35
+   name: 'John Smith',
+   age: 35,
 };
 
 user = JSON.stringify(user);
@@ -234,22 +235,23 @@ console.log(user2); // { name: 'John Smith', age: 35 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // TASK 2 - Write replacer function to stringify everything, but remove properties that reference meetup
-
 let room = {
-    number: 23,
+   number: 23,
 };
 
 let meetup = {
-    title: 'Conference',
-    occupiedBy: [{ name: 'Jack' }, { name: 'Alice' }],
-    place: room,
+   title: 'Conference',
+   occupiedBy: [{ name: 'Jack' }, { name: 'Alice' }],
+   place: room,
 };
 
 room.occupiedBy = meetup;
 meetup.self = meetup;
 
-console.log(JSON.stringify(meetup, function replacer(key, value) {
-    return (key != "" && value == meetup) ? undefined : value
-}));
+console.log(
+   JSON.stringify(meetup, function replacer(key, value) {
+      return key != '' && value == meetup ? undefined : value;
+   }),
+);
 
 // we also need to test key=="" to exclude the first call where it is normal that value is meetup
