6a24efbaa3c0a532eee7fec560dc73c95af560fe
Michalina <michalina.gol@gmail.com>
2018-11-30 17:50:54 +0100

    Add informations about rest parameters

---
+++ rest_spread.js
@@ -0,0 +1,100 @@
+// REST PARAMETERS
+// collects all remaining elements into an array
+function add(x, y) {
+   return x + y;
+}
+
+// In Javascript it is possible to call a function with any number of arguments. However, only the fist two arguments will be counted.
+console.log(add(1, 2, 3, 4, 5)); // 3
+
+// With rest parameters we can gather any number of arguments into an array and do what we want with them
+function add2(...args) {
+   let result = 0;
+
+   for (let arg of args) {
+      result += arg;
+   }
+
+   return result;
+}
+
+console.log(add2(1)); // 1
+console.log(add2(1, 2)); // 3
+console.log(add2(1, 2, 3, 4, 5)); // 15
+
+//--------------------REMEMBER-------------------
+// Rest parameters have to be at the last argument
+/*  function f(arg1, ...rest, arg2) {
+        // arg2 after ...rest ?!
+        // Error: Rest parameter must be last formal parameter
+    }
+*/
+
+function showName(firstName, lastName, ...titles) {
+   console.log(firstName + ' ' + lastName); // Julius Caesar
+
+   console.log(titles[0]); // Consul
+   console.log(titles[1]); // Imperator
+   console.log(titles.length); // 2
+}
+
+showName('Julius', 'Caesar', 'Consul', 'Imperator');
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+// Before rest parameters existed, to get all the arguments in a function we used arguments which is an array-like object.
+function someFunction() {
+   return arguments;
+}
+
+console.log(someFunction('joykare', 100, false)); // [Arguments] { '0': 'joykare', '1': 100, '2': false }
+
+function showName() {
+   console.log(arguments.length + ', ' + arguments[0] + ', ' + arguments[1]);
+}
+
+showName('Julius', 'Cesar'); // 2, Julius, Cesar
+showName('Ilya'); // 1, Ilya, undefined
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+// Arrow functions do not have "arguments"
+// As you remember, arrow functions don’t have their own this
+function f() {
+   // If we access the arguments object from an arrow function, it takes them from the outer “normal” function.
+   let showArg = () => console.log(arguments[0]);
+   showArg();
+}
+
+f(1); // 1
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// SPREAD OPERATOR
+// allows iterables( arrays / objects / strings ) to be expanded into single arguments/elements
+
+// Adding array elements to an existing array
+let arr = ['Apple', 'Lemon', 'Orange'];
+let newArr = ['Banana', ...arr];
+
+console.log(newArr); // [ 'Banana', 'Apple', 'Lemon', 'Orange' ]
+
+// Copying arrays
+let arr = [1, 2, 3];
+let newArr = [...arr];
+
+console.log(newArr); // [ 1, 2, 3 ]
+
+// Pass elements of an array to a function as separate arguments
+function add(a, b, c) {
+   return a + b + c;
+}
+
+let args = [1, 2, 3];
+
+console.log(add(...args)); // 6
+
+// Spread operator works with any iterable
+let str = 'banana';
+
+console.log([...str]); // [ 'b', 'a', 'n', 'a', 'n', 'a' ]
