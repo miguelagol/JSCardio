6f5e31fb01863e579bb1fb375a64ceede7ebfe74
miguelagol <michalina.gol@gmail.com>
2019-03-07 22:11:08 +0100

    Add informations about Error Object and throwing our own errors

--- error.js
+++ error.js
@@ -69,6 +69,111 @@ setTimeout(function() {
    }
 }, 1000); // Error is caught here!
 
+//-----------------------------------------------------------------------------------------------------------------------------------------
 
+// Error object
+/* When an error occurs, JavaScript generates an object containing the details about it.
+   The object is then passed as an argument to catch */
+
+/* For all built-in errors, the error object inside catch block has two main properties:
+   -  name  -  Error name. For an undefined variable that’s "ReferenceError".
+   -  message  -  Textual message about error details.
+
+   There are other non-standard properties available in most environments. One of most widely used and supported is:
+   -  stack -  Current call stack: a string with information about the sequence of nested calls that led to the error.
+      Used for debugging purposes.
+*/
+
+try {
+   lalala;
+} catch (error) {
+   console.log(error.name); // ReferenceError
+   console.log(error.message); // lalala is not defined
+   console.log(error.stack); // ReferenceError: lalala is not defined
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+// Optional 'catch' binding
+/* If we don’t need error details, catch may omit it
+   try {
+      // ...
+   } catch {
+      // error object omitted
+   }
+*/
+
+// valid code
+let json = '{"name":"John", "age": 30}'; // data from the server
+
+let user = JSON.parse(json); // convert the text representation to JS object
+
+// now user is an object with properties from the string
+console.log(user.name); // John
+console.log(user.age); // 30
+
+// If json is malformed, JSON.parse generates an error, so the script “dies”.
+let json = '{bad json}';
+
+try {
+   let user = JSON.parse(json);
+   console.log(user.name);
+} catch (error) {
+   console.log(
+      "Our apologies, the data has error, we'll try to request it one more time.",
+   );
+   console.log(error.name); // SyntaxError
+   console.log(error.message); // Unexpected token b in JSON at position 1
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+// Throwing our own errors
+let json = '{ "age": 30 }';
+
+try {
+   let user = JSON.parse(json);
+   console.log(user.name); // undefined
+} catch (error) {
+   console.log("doesn't execute");
+}
+
+// To unify error handling, we’ll use the throw operator.
+// 'Throw' operator
+// the throw operator generates an error
+// throw <error object>
+/* We can use anything as an error object. That may be even a primitive, like a number or a string,
+   but it’s better to use objects, preferably with name and message properties 
+   JavaScript has many built-in constructors for standard errors: Error, SyntaxError, ReferenceError, TypeError and others.
+   We can use them to create error objects as well.
+*/
+let error = new Error(message);
+let error2 = new SyntaxError(message);
+let error3 = new ReferenceError(message);
+/* For built-in errors (not for any objects, just for errors), the name property is exactly the name of the constructor.
+   And message is taken from the argument.
+*/
+let error = new Error('Things happen o_O');
+
+console.log(error.name); // Error
+console.log(error.message); // Things happen o_O
+
+let json = '{ "age": 30 }';
+
+try {
+   let user = JSON.parse(json);
+
+   if(!user.name) {
+      throw new SyntaxError('Incomplete data: no name');
+   }
+
+   console.log(user.name);
+} catch (error) {
+   console.log('JSON Error: ' + error.message); // JSON Error: Incomplete data: no name
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+// Rethrowing
 
 
