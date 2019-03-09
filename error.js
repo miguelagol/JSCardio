cde3fb91b334d1c98cc60ea72f297f192988229c
miguelagol <michalina.gol@gmail.com>
2019-03-10 00:26:29 +0100

    Add solution of one task

--- error.js
+++ error.js
@@ -587,3 +587,22 @@ function f() {
 }
 
 console.log(f()); // start    cleanup!    Error: An error
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+// TASK 2 - Inherit from SyntaxError
+class FormatError extends SyntaxError {
+   constructor(message) {
+      super(message);
+      this.name = 'FormatError';
+   }
+}
+
+let err = new FormatError('formatting error');
+
+console.log(err.message); // formatting error
+console.log(err.name); // FormatError
+console.log(err.stack); // FormatError: formatting error at Object...
+
+console.log(err instanceof FormatError); // true
+console.log(err instanceof SyntaxError); // true
