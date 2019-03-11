596ed79b1350619b5d97a8c282e5d968a1b5bfd7
miguelagol <michalina.gol@gmail.com>
2019-03-11 18:25:06 +0100

    Add informations about Promise Object

--- promises.js
+++ promises.js
@@ -133,6 +133,59 @@ function step3(error, script) {
 
 //-----------------------------------------------------------------------------------------------------------------------------------------
 
+// Promise Object
+/* Syntax:
+  let promise = new Promise(function(resolve, reject) {
+    // executor (the producing code)
+  });
+*/
+/*  The function passed to new Promise is called the executor. When the promise is created, this executor function runs automatically.
+    It contains the producing code, that should eventually produce a result.
+
+  The resulting promise object has internal properties:
+    - state — initially “pending”, then changes to either “fulfilled” or “rejected”,
+    - result — an arbitrary value of your choosing, initially undefined.
+
+  When the executor finishes the job, it should call one of the functions that it gets as arguments:
+    - resolve(value) — to indicate that the job finished successfully:
+        sets state to "fulfilled",
+        sets result to value.
+    - reject(error) — to indicate that an error occurred:
+        sets state to "rejected",
+        sets result to error.
+*/
+// Fulfilled promise
+let promise = new Promise(function(resolve, reject) {
+  // The executor receives two arguments: resolve and reject — these functions are pre-defined by the JavaScript engine.
+  // We should write the executor to call them when ready.
+  setTimeout(() => resolve('done!'), 1000);
+}); // calls resolve('done') ->  state: 'fulfilled'  result: 'done'
+
+// Rejected promise
+let promise = new Promise(function(resolve, reject) {
+  setTimeout(() => reject(new Error('Whooops!'), 1000));
+}); // calls reject(error) ->   state: 'rejected'   result: error
+
+/*  The executor should do a job (something that takes time usually) and then call resolve or reject to change the state
+    of the corresponding Promise object.
+    The Promise that is either resolved or rejected is called “settled”, as opposed to a “pending” Promise.
+*/
+
+//--------------------REMEMBER--------------------
+//  - There can be only a single result or an error
+//    The promise's state change is final. Alll further calls of resolve and reject are ignored
+let promise = new Promise(function(resolve, reject) {
+  resolve('done');
+  reject(new Error('...')); // ignored
+  setTimeout(() => resolve('...')); // ignored
+});
+
+//  - Reject with Error objects
+//    In case something goes wrong, we can call reject with any type of argument (just like resolve).
+//    But it is recommended to use Error objects (or objects that inherit from Error). 
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
 // TASK 1 - Animated circle with callback
 {/* <style>
    .circle {
