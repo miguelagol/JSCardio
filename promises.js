8f2f9303ce326a3de452cae26628acc111fb3bd0
miguelagol <michalina.gol@gmail.com>
2019-03-19 16:38:04 +0100

    Add solutions of 3 tasks

--- promises.js
+++ promises.js
@@ -1575,3 +1575,129 @@ Promise.all(requests)
    TypeError
    SyntaxError
 */
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+// TASK 9 - Rewrite using async/await
+function loadJson(url) {
+   return fetch(url)
+     .then(response => {
+       if (response.status == 200) {
+         return response.json();
+       } else {
+         throw new Error(response.status);
+       }
+     })
+ }
+ 
+loadJson('no-such-user.json') // (3)
+   .catch(alert); // Error: 404
+
+async function loadJson(url) {
+   let response = await fetch(url);
+   
+   if(response.status === 200){
+      let json = await response.json();
+      return json;    
+   } else {
+      throw new Error(response.status)
+   }
+}
+
+loadJson('no-such-user.json')
+   .catch(console.log); // Error: 404
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+// TASK 10 - Rewrite 'rethrow' async/await
+class HttpError extends Error {
+   constructor(response) {
+     super(`${response.status} for ${response.url}`);
+     this.name = 'HttpError';
+     this.response = response;
+   }
+}
+ 
+function loadJson(url) {
+   return fetch(url)
+      .then(response => {
+      if (response.status == 200) {
+         return response.json();
+      } else {
+         throw new HttpError(response);
+      }
+   })
+}
+ 
+// Ask for a user name until github returns a valid user
+function demoGithubUser() {
+   let name = prompt("Enter a name?", "iliakan");
+ 
+   return loadJson(`https://api.github.com/users/${name}`)
+      .then(user => {
+         alert(`Full name: ${user.name}.`);
+         return user;
+      })
+      .catch(err => {
+         if (err instanceof HttpError && err.response.status == 404) {
+            alert("No such user, please reenter.");
+            return demoGithubUser();
+         } else {
+            throw err;
+         }
+      });
+}
+ 
+demoGithubUser();
+
+class HttpError extends Error {
+   constructor(response) {
+     super(`${response.status} for ${response.url}`);
+     this.name = 'HttpError';
+     this.response = response;
+   }
+ }
+ 
+async function loadJson(url) {
+   let response = await fetch(url);
+
+   if (response.status == 200) {
+      let json = await response.json();
+      return json;
+   } else {
+      throw new HttpError(response);
+   }
+}
+
+async function demoGithubUser() {
+   let name = prompt("Enter a name?", "iliakan");
+
+   try {
+      let user = await loadJson(`https://api.github.com/users/${name}`)
+      alert(`Full name: ${user.name}.`);
+   } catch (error){
+      if (error instanceof HttpError && error.response.status == 404) {
+         alert("No such user, please reenter.");
+         return demoGithubUser();
+      } else {
+         throw error;
+      }
+   }
+}
+ 
+demoGithubUser();
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+// TASK 11 - Call async from non-async
+// How to call async from function wait and use its result?
+async function wait() {
+   await new Promise(resolve => setTimeout(resolve, 1000));
+   return 10;
+}
+
+function func() {
+   wait()
+      .then(console.log)
+}
+
+func(); // 10
