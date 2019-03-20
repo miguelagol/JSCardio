1cbc680ab214635faf1ab74386016df259d6dac4
miguelagol <michalina.gol@gmail.com>
2019-03-20 14:49:07 +0100

    Add informations about iterable generator

--- generators.js
+++ generators.js
@@ -45,7 +45,7 @@ console.log(JSON.stringify(two)); // {"value":2,"done":false}
 
 let three = generator.next();
 
-console.log(JSON.stringify(three)) // {"value":3,"done":true}
+console.log(JSON.stringify(three)); // {"value":3,"done":true}
 
 // If we make the new call generator.next() they return the same object: {done, true}
 // There’s no way to “roll back” a generator. But we can create another one by calling generateSequence().
@@ -63,14 +63,108 @@ console.log(JSON.stringify(three)) // {"value":3,"done":true}
 //-----------------------------------------------------------------------------------------
 
 // Generators are iterable
+function* generateSequence() {
+   yield 1;
+   yield 2;
+   return 3;
+}
+
+let generator = generateSequence();
+
+for (let value of generator) {
+   console.log(value); // 1, 2
+}
+
+// It doesn’t show 3!
+// for..of iteration ignores the last value, when done: true
+// if we want all results to be shown, we must return them with yield
+function* generateSequence() {
+   yield 1;
+   yield 2;
+   yield 3;
+}
+
+let generator = generateSequence();
+
+for (let value of generator) {
+   console.log(value); // 1, 2, 3
+}
+
+// We can call iterable functionality
+function* generateSequence() {
+   yield 1;
+   yield 2;
+   yield 3;
+}
+
+let sequence = [0, ...generateSequence()];
+
+console.log(sequence); // [ 0, 1, 2, 3 ]
 
 //-----------------------------------------------------------------------------------------
 
 // Using generators instead of iterables
 
+// iterables version
+let range = {
+   from: 1,
+   to: 5,
+
+   // for..of calls this method once in the very beginning
+   [Symbol.iterator]() {
+      // it returns the iterator object
+      return {
+         current: this.from,
+         last: this.to,
+
+         // next() is called on each iteration by the for..of loop
+         next() {
+            if (this.current <= this.last) {
+               return { done: false, value: this.current++ };
+            } else {
+               return { done: true };
+            }
+         },
+      };
+   },
+};
+
+console.log([...range]); // [ 1, 2, 3, 4, 5 ]
+
+// generators version
+function* generateSequence(start, end) {
+   for (let i = start; i <= end; i++) {
+      yield i;
+   }
+}
+
+let sequence = [...generateSequence(1, 5)];
+
+console.log(sequence); // [ 1, 2, 3, 4, 5 ]
+
 //-----------------------------------------------------------------------------------------
 
 // Converting Symbol.iterator to generator
+let range = {
+   from: 1,
+   to: 5,
+
+   *[Symbol.iterator]() {
+      // a shorthand for [Symbol.iterator]: function*()
+      for (let value = this.from; value <= this.to; value++) {
+         yield value;
+      }
+   },
+};
+
+console.log([...range]); // [ 1, 2, 3, 4, 5 ]
+// The range object is now iterable
+
+/* That works pretty well, because when range[Symbol.iterator] is called:
+   -  it returns an object (now a generator)
+   -  that has .next() method
+   -  that returns values in the form {value: ..., done: true/false}
+*/
 
 //-----------------------------------------------------------------------------------------
 
@@ -85,4 +179,3 @@ console.log(JSON.stringify(three)) // {"value":3,"done":true}
 // generator.throw
 
 //-----------------------------------------------------------------------------------------------------------------------------------------
-
